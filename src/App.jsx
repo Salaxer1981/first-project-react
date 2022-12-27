07a6bde09ff6ebd9adfe708df5644affe80ad39b
slaxer1981 <alexandercedeno@gmal.com>
2022-12-27 00:06:51 -0300

    Cambios finales

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,7 @@ function App() {
 
   return (
     <div className="App">
-      <RandomCard
-      />
-      
+      <RandomCard/>   
     </div>
   )
 }
--- src/Componentes/randomCard.jsx
+++ src/Componentes/randomCard.jsx
@@ -20,34 +20,37 @@ const RandomCard = () => {
     setColors(newColors);
     }
 
-     
-
-    
-    
-
-    
-
-
-
    return (
       <div className='contenedor-general' style={{background:colors[indexColors]}} >
             <div className='firstContainer' style={{color:colors[indexColors]}}>
               <h1>{`${users[index]?.name.title} ${users[index]?.name.first} ${users[index]?.name.last}`}</h1>
 
               <div className='imagen'>
-              <img className='imagen-perfil' src={users[index].picture.large} alt="imagen" />
+                 <img className='imagen-perfil' src={users[index].picture.large} alt="imagen"/>
               </div>
             
               <ul className='dataList'>
-                 <li className='item'> <img className='icon' src="/mi.svg" alt="" /> {users[index].email} </li>
-                 <li className='item'> <img className='icon' src="/mi.svg" alt="" />{users[index].phone}</li>
-                 <li className='item'> <img className='icon' src="/mi.svg" alt="" />{`${users[index].location.country} ${users[index].location.state} ${users[index].location.city}`}</li>
+                  <li className='item'>
+                     <img className='icon' style={{background:colors[indexColors]}} src="https://img.icons8.com/fluency/48/null/composing-mail.png" alt="icono"/>  
+                     {users[index].email} 
+                  </li>
+                 
+                  <li className='item'>
+                     <img className='icon' style={{background:colors[indexColors]}} src="https://img.icons8.com/flat-round/64/null/phone.png" alt="icono"/>
+                     {users[index].phone}
+                  </li>
+                  
+                  <li className='item'> 
+                     <img className='icon' style={{background:colors[indexColors]}} src="https://img.icons8.com/ios/50/null/marker-o.png" alt="icono"/>
+                     {`${users[index].location.country} ${users[index].location.state} ${users[index].location.city}`}
+                  </li>
+
                </ul>
             </div>
 
             <div>
-              <button onClick={Change}>
-                siguiente
+              <button className='button' onClick={Change}>
+                Next
               </button>
             </div>
 
